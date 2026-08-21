d2c2a6365fc19428428e15ba329804f12b879ef7
zenvioralabs <admin@zenvioralabs.com>
2026-08-21 17:35:29 +0530

    Update hero.ts

--- content/hero.ts
+++ content/hero.ts
@@ -5,11 +5,13 @@ export interface HeroContent {
   supportingStatement: string;
 }
 
+
+
 export const hero: HeroContent = {
   taglineOptions: ["Where Ideas Gain Momentum"],
   headlineLines: ["A Partner for", "Technology and", "Business Intelligence"],
   subheading:
     "MField Labs helps growing businesses use technology and data better, enabling more efficient operations and confident, informed decisions.",
-  //supportingStatement:
-   // "From visibility to velocity, from insight to action — we engineer the capabilities that enable organizations to scale confidently in a rapidly evolving world.",
+  supportingStatement:
+    "",
 };
